89c27e7c9fb9a5b01cb8f0880bc972517d61e695
Cotes Chung <cotes.chung@gmail.com>
2019-11-25 00:19:03 +0800

    Optimized JS to hide empty ToC.

--- assets/js/_src/toc.js
+++ assets/js/_src/toc.js
@@ -6,8 +6,11 @@
  */
 
 $(function() {
-  // Hide ToC title if there is no head
-  if ($("#toc-wrap>nav#toc>ul>li").length == 0) {
-    $("#toc-wrap>h3").addClass("hidden");
+  if ($("#post-wrap .post-content h1").length == 0
+      && $("#post-wrap .post-content h2").length == 0
+      && $("#post-wrap .post-content h3").length == 0
+      && $("#post-wrap .post-content h4").length == 0
+      && $("#post-wrap .post-content h5").length == 0) {
+    $("#toc-wrap").addClass("hidden");
   }
 });
