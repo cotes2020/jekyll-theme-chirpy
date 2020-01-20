4ef83b9e6016157b3413747f8affc0d8d9a245a0
Cotes Chung <cotes.chung@gmail.com>
2020-01-21 00:32:20 +0800

    Updated the CI config.

---
+++ tools/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Using HTML-proofer to test site.
+#
+# Requirement: https://github.com/gjtorikian/html-proofer
+#
+# Usage: bash /path/to/test.sh
+#
+# v2.0
+# https://github.com/cotes2020/jekyll-theme-chirpy
+# © 2020 Cotes Chung
+# MIT Licensed
+
+DEST=_site
+URL_IGNORE=cdn.jsdelivr.net
+
+bundle exec htmlproofer $DEST \
+  --disable-external \
+  --check-html \
+  --empty_alt_ignore \
+  --allow_hash_href \
+  --url_ignore $URL_IGNORE
