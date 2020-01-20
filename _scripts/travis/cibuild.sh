86c324dd707e2d50f1befdfe31835369da42a62d
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2020-01-21 00:32:20 +0800

    Updated the CI config.

--- _scripts/travis/cibuild.sh
+++ _scripts/travis/cibuild.sh
@@ -8,30 +8,15 @@
 
 if [[ $TRAVIS_PULL_REQUEST = "false" ]]; then # triggered by author
 
-  BUILDER=../blog-builder
+  git clone https://${GH_PAT}@github.com/${GH_USER}/${BUILDER_REPO}.git \
+    ../${BUILDER_REPO} --depth=1 -q
 
-  git clone https://${GH_TOKEN}@github.com/cotes2020/blog-builder.git \
-    $BUILDER --depth=1 -q
-
-  cp -r $BUILDER/framework/* .
+  cp -r ../${BUILDER_REPO}/framework/* .
   bash _cibuild.sh
 
 else # triggered by Pull Request
 
-  SAFE_DOMAIN="cdn.jsdelivr.net"
-
-  python _scripts/py/init_all.py
-
-  build_cmd="bundle exec jekyll build"
-
-  echo "\$ $build_cmd"
-  eval $build_cmd
-
-  bundle exec htmlproofer _site/ \
-            --disable-external \
-            --check-html \
-            --empty_alt_ignore \
-            --allow_hash_href \
-            --url_ignore $SAFE_DOMAIN
+  bash tools/build.sh
+  bash tools/test.sh
 
 fi
