45fbbbab49bdc400471d9c4dc20350b672e70c26
Cotes Chung <cotes.chung@gmail.com>
2020-02-17 03:01:48 +0800

    Feature: Added post sharing options.
     Also added a license statement at the bottom of the posts.

---
+++ assets/js/_src/_commons/copy-link.js
@@ -0,0 +1,19 @@
+/*
+* Copy current page url to clipboard.
+* v2.1
+* https://github.com/cotes2020/jekyll-theme-chirpy
+* © 2020 Cotes Chung
+* MIT License
+*/
+
+function copyLink() {
+  var url = window.location.href;
+  var $temp = $("<input>");
+
+  $("body").append($temp);
+  $temp.val(url).select();
+  document.execCommand("copy");
+  $temp.remove();
+
+  alert("Link copied successfully!");
+}
