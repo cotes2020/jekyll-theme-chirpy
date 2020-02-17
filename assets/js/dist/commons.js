6728ed85c5910c32fbbfdc9594c7fd25133015ab
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2020-02-18 00:17:42 +0800

    Optimized the JS import sequence.

--- assets/js/dist/commons.js
+++ assets/js/dist/commons.js
@@ -9,5 +9,4 @@
 {% include_relative _commons/sidebar.min.js %}
 {% include_relative _commons/topbar-switch.min.js %}
 {% include_relative _commons/topbar-title.min.js %}
-{% include_relative _commons/tooltip.min.js %}
 {% include_relative _commons/copy-link.min.js %}
