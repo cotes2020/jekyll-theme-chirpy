15a98aca70ff5d4cb3d96d37682ed49abc41455f
Cotes Chung <cotes.chung@gmail.com>
2020-08-19 17:05:58 +0800

    Improve the Shell code style.

--- tools/pv.sh
+++ tools/pv.sh
@@ -15,12 +15,12 @@
 
 set -eu
 
-WORK_DIR=$(dirname $(dirname $(realpath "$0")))
-URL_FILE=${WORK_DIR}/_config.yml
-PV_CACHE=${WORK_DIR}/assets/js/data/pageviews.json
+WORK_DIR="$(dirname $(dirname $(realpath "$0")))"
+URL_FILE="${WORK_DIR}/_config.yml"
+PV_CACHE="${WORK_DIR}/assets/js/data/pageviews.json"
 
-PROXY_URL=$(grep "proxy_endpoint:" $URL_FILE | sed "s/.*: '//g;s/'.*//")
+PROXY_URL="$(grep "proxy_endpoint:" "$URL_FILE" | sed "s/.*: '//g;s/'.*//")"
 
-wget $PROXY_URL -O $PV_CACHE
+wget "$PROXY_URL" -O "$PV_CACHE"
 
 echo "ls $PV_CACHE"
