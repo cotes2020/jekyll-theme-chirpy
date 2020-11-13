a3604742e3d9f8cc2aa5818c48bbfdbbd30adf46
wolfgangofner@aon.at <WolfgangOfner@aon.at>
2020-11-13 21:17:54 +0100

    replace remaining files

--- tools/test.ps1
+++ tools/test.ps1
@@ -28,33 +28,32 @@ foreach ($filename in $filenames)
 }
 
 echo "XML files replaced"
-#
-#echo "Searching JSON files"
-#
-#$fileNames = Get-ChildItem -Path "$($PWD)\*.JSON" -Recurse | select -expand fullname
-#
-#echo "$($fileNames.Length) files found"
-#echo "Replacing http://0.0.0.0:4000 with https://www.programmingwithwolfgang.com in HTML files"
-#
-#foreach ($filename in $filenames) 
-#{
-# (  Get-Content $fileName) -replace "http://0.0.0.0:4000","https://www.programmingwithwolfgang.com" | Set-Content $fileName -Force
-#}
-#
-#echo "JSON files replaced"
-#
-#echo "Searching TXT files"
-#
-#$fileNames = Get-ChildItem -Path "$($PWD)\*.txt" -Recurse | select -expand fullname
-#
-#echo "$($fileNames.Length) files found"
-#echo "Replacing http://0.0.0.0:4000 with https://www.programmingwithwolfgang.com in HTML files"
-#
-#foreach ($filename in $filenames) 
-#{
-# (  Get-Content $fileName) -replace "http://0.0.0.0:4000","https://www.programmingwithwolfgang.com" | Set-Content $fileName -Force
-#}
-#
-#echo "TXT files replaced"
-#
-#
+
+echo "Searching JSON files"
+
+$fileNames = Get-ChildItem -Path "$($PWD)\*.JSON" -Recurse | select -expand fullname
+
+echo "$($fileNames.Length) files found"
+echo "Replacing http://0.0.0.0:4000 with https://www.programmingwithwolfgang.com in HTML files"
+
+foreach ($filename in $filenames) 
+{
+ (  Get-Content $fileName) -replace "http://0.0.0.0:4000","https://www.programmingwithwolfgang.com" | Set-Content $fileName -Force
+}
+
+echo "JSON files replaced"
+
+echo "Searching TXT files"
+
+$fileNames = Get-ChildItem -Path "$($PWD)\*.txt" -Recurse | select -expand fullname
+
+echo "$($fileNames.Length) files found"
+echo "Replacing http://0.0.0.0:4000 with https://www.programmingwithwolfgang.com in HTML files"
+
+foreach ($filename in $filenames) 
+{
+ (  Get-Content $fileName) -replace "http://0.0.0.0:4000","https://www.programmingwithwolfgang.com" | Set-Content $fileName -Force
+}
+
+echo "TXT files replaced"
+
