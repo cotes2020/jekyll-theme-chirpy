f0b9150829b6dc5d0387126bcbf9de03aa9f5912
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2020-11-27 01:51:15 +0800

    Improve the bump version tool

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -11,18 +11,43 @@ set -eu
 
 META_FILE="_data/meta.yml"
 
-_latest_tag="$(git describe --abbrev=0)"
+bump() {
+  _version_field="version: $1"
 
-_version_field="version: $_latest_tag"
+  if [[ ! -f $META_FILE ]]; then
+    echo "name: Chirpy" > $META_FILE
+    echo "$_version_field" >> $META_FILE
+    echo "homepage: https://github.com/cotes2020/jekyll-theme-chirpy/" >> $META_FILE
+  else
+    sed -i "s/^version:.*/$_version_field/g" $META_FILE
+  fi
 
-if [[ ! -f $META_FILE ]]; then
-  echo "name: Chirpy" > $META_FILE
-  echo "$_version_field" >> $META_FILE
+  if [[ -n $(git status $META_FILE -s) ]]; then
+    git add $META_FILE
+    git commit -m "Bump version to $1"
+  fi
+}
+
+_latest_tag="$(git describe --tags --abbrev=0)"
+
+echo "Input a version number (hint: latest version is ${_latest_tag:1})"
+
+read version
+
+if [[ $version =~ ^[[:digit:]]+\.[[:digit:]]+(\.[[:digit:]]+)?$ ]]; then
+
+  if git tag --list | egrep -q "^v$version$"; then
+    echo "Error: version '$version' already exists"
+    exit -1
+  fi
+
+  echo "Bump version to $version"
+  bump "$version"
+
+  echo "Create tag v$version"
+  git tag "v$version"
+
 else
-  sed -i "s/^version:.*/$_version_field/g" $META_FILE
-fi
 
-if [[ -n $(git status $META_FILE -s) ]]; then
-  git add $META_FILE
-  git commit -m "Bump version to $_latest_tag"
+  echo "Error: Illegal version number: '$version'"
 fi
