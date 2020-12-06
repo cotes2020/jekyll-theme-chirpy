8d4c5556386512eb134c172e321b32ebaf8eb3b8
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2020-12-06 23:12:20 +0800

    Fix gh-actions cache not being hit issue (#191)

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -15,6 +15,16 @@ _no_branch=false
 _backup_dir="$(mktemp -d)"
 
 init() {
+  if [[ -z ${GITHUB_ACTION+x} ]]; then
+    echo "ERROR: This script is not allowed to run outside of GitHub Action."
+    exit -1
+  fi
+
+  # Gemfile could be changed by `bundle install` in actions workflow
+  if [[ -n $(git status Gemfile.lock --porcelain) ]]; then
+    git checkout -- Gemfile.lock
+  fi
+
   if [[ -z $(git branch -av | grep "$PAGES_BRANCH") ]]; then
     _no_branch=true
     git checkout -b "$PAGES_BRANCH"
