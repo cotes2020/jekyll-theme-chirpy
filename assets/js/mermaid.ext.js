c47421a94c31fcaa8052c6c60cdcd92ccf2ac63d
Soubhik Biswas <55374257+s0ubhik@users.noreply.github.com>
2020-12-07 15:04:34 +0530

    Update mermaid.ext.js

--- assets/js/mermaid.ext.js
+++ assets/js/mermaid.ext.js
@@ -8,34 +8,35 @@
 */
 class _mermaid {
 	constructor(Class){
-		if (Class == undefined) Class = "mermaid"
-		this.graphs = []
-		let mrmds  = document.getElementsByClassName(Class)
-		this.mrmds = mrmds
-        let graps = []
+		if (Class === undefined){
+      Class = "mermaid";
+    }
+		this.graphs = [];
+		let mrmds  = document.getElementsByClassName(Class);
+		this.mrmds = mrmds;
+        let graps = [];
         for (let i=0; i < mrmds.length; i++){
-	      let mrmd = mrmds[i]
-	      let ID = "Mermaid-"+i
-	      let graph = mrmd.innerText
-	      mrmd.setAttribute("id",ID)
-	      this.graphs.push({id: ID, graph: graph})
+	        mrmds[i].setAttribute("id",ID);
+	        this.graphs.push({id: "Mermaid-"+i, graph: mrmds[i].innerText});
         }
 	}
 
 	async clean(){
 		for (let i=0; i < this.mrmds.length; i++){
-			if (this.mrmds[i].getAttribute("id") == this.graphs[i].id){
-				this.mrmds[i].innerText = this.graphs[i].graph
-				this.mrmds[i].removeAttribute("data-processed")
+			if (this.mrmds[i].getAttribute("id") === this.graphs[i].id){
+				this.mrmds[i].innerText = this.graphs[i].graph;
+				this.mrmds[i].removeAttribute("data-processed");
 			}
 		}
 	}
 
 	async render(init){
-		await this.clean()
-		if (init == null) init = {}
-		mermaid.initialize(init)
-		mermaid.init(init, this.mrmds)
+		await this.clean();
+		if (init === undefined){
+      init = {};
+    };
+		mermaid.initialize(init);
+		mermaid.init(init, this.mrmds);
 		
 	}
 }
