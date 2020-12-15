d8a5a29564e57c9b428106fd7c35d51ed9702541
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2020-12-15 15:20:03 +0800

    Simplify checkbox with fontawesome icon

--- assets/js/_commons/checkbox.js
+++ assets/js/_commons/checkbox.js
@@ -6,7 +6,7 @@ $(function() {
   /* hide browser default checkbox */
   $("input[type=checkbox]").addClass("unloaded");
   /* create checked checkbox */
-  $("input[type=checkbox][checked]").before("<span checked></span>");
+  $("input[type=checkbox][checked]").before("<i class=\"fas fa-check-circle checked\"></i>");
   /* create normal checkbox */
-  $("input[type=checkbox]:not([checked])").before("<span></span>");
+  $("input[type=checkbox]:not([checked])").before("<i class=\"far fa-circle\"></i>");
 });
