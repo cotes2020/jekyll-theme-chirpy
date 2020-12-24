1bff91580744c9797a1864d3b6b104af259272fd
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2020-12-24 22:51:08 +0800

    Jump to the anchor position smoothly

--- assets/js/_commons/back-to-top.js
+++ assets/js/_commons/back-to-top.js
@@ -1,9 +1,8 @@
 /*
 Reference: https://bootsnipp.com/snippets/featured/link-to-top-page
 */
-
 $(function() {
-  $(window).scroll(function() {
+  $(window).scroll(() => {
     if ($(this).scrollTop() > 50 &&
       $("#sidebar-trigger").css("display") === "none") {
       $("#back-to-top").fadeIn();
@@ -12,10 +11,8 @@ $(function() {
     }
   });
 
-  $("#back-to-top").click(function() {
-    $("body,html").animate({
-      scrollTop: 0
-    }, 800);
+  $("#back-to-top").click(() => {
+    $("body,html").scrollTop(0);
     return false;
   });
 });
