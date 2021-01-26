257dac7f96b49bb3d0aa6907f02c847537b2aa36
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2021-01-26 22:28:35 +0800

    Bump version to 3.0.1

--- assets/js/dist/home.min.js
+++ assets/js/dist/home.min.js
@@ -1,5 +1,5 @@
 /*!
- * Chirpy v3.0.0 (https://github.com/cotes2020/jekyll-theme-chirpy/)
+ * Chirpy v3.0.1 (https://github.com/cotes2020/jekyll-theme-chirpy/)
  * © 2021 Cotes Chung
  * MIT Licensed
  */
