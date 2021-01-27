3d6955021fea4de856d53c5fdce3e997cbe1007e
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2021-01-28 03:37:08 +0800

    Fix the year of copyright

--- assets/js/data/cache-list.js
+++ assets/js/data/cache-list.js
@@ -2,10 +2,6 @@
 layout: compress
 
 # The list to be cached by PWA
-# Chirpy v2.2
-# https://github.com/cotes2020/jekyll-theme-chirpy
-# © 2020 Cotes Chung
-# MIT Licensed
 ---
 
 const include = [
