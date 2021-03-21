18f6c6ac4e142aad9a190aa236b7adf7f91adaba
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2021-03-21 15:55:04 +0800

    Fix PWA error caused by service worker store the POST Request as cache key
    fix #297, fix #301

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ self.importScripts('{{ "/assets/js/data/cache-list.js" | relative_url }}');
 
 var cacheName = 'chirpy-{{ "now" | date: "%Y%m%d.%H%M" }}';
 
-
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
   for (const rule of exclude) {
@@ -19,7 +18,6 @@ function isExcluded(url) {
   return false;
 }
 
-
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
@@ -29,24 +27,25 @@ self.addEventListener('install', (e) => {
   );
 });
 
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
-      /* console.log('[Service Worker] Fetching resource: ' + e.request.url); */
+      /* console.log(`[sw] method: ${e.request.method}, fetching: ${e.request.url}`); */
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
           if (!isExcluded(e.request.url)) {
-            /* console.log('[Service Worker] Caching new resource: ' + e.request.url); */
-            cache.put(e.request, response.clone());
+            if (e.request.method === "GET") {
+              /* console.log('[sw] Caching new resource: ' + e.request.url); */
+              cache.put(e.request, response.clone());
+            }
           }
           return response;
         });
+
       });
     })
   );
 });
-
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
