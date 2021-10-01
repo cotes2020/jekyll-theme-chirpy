ea3b7bdc1db37eef33348edf8f2523b88d9bc69f
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2021-10-01 18:48:42 +0800

    Bump version to 4.3.1

--- assets/js/dist/pvreport.min.js
+++ assets/js/dist/pvreport.min.js
@@ -1,5 +1,5 @@
 /*!
- * Chirpy v4.3.0 (https://github.com/cotes2020/jekyll-theme-chirpy/)
+ * Chirpy v4.3.1 (https://github.com/cotes2020/jekyll-theme-chirpy/)
  * © 2019 Cotes Chung
  * MIT Licensed
  */
