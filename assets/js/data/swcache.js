4a2016a9e39460a6b1c342cd0367bad575465a15
Tamme Schichler <tamme@schichler.dev>
2022-01-14 12:01:22 +0100

    Don't include Google Analytics scripts without analytics id (#482)

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -43,7 +43,7 @@ const resource = [
 
 /* The request url with below domain will be cached */
 const allowedDomains = [
-  {% if site.google_analytics.id != '' %}
+  {% if site.google_analytics.id != empty and site.google_analytics.id %}
     'www.googletagmanager.com',
     'www.google-analytics.com',
   {% endif %}
