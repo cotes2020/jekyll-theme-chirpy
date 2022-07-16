41d68e9e2d51e12f8ebe933123b0d3c048bd6de5
Qin Li <liblaf@outlook.com>
2022-07-17 05:03:45 +0800

    feat(math): add more useful packages
    Add more useful packages like "ams", "textmacros", "physics", etc.
    Support convenient configuration of auto numbering math equations.

--- assets/js/math.js
+++ assets/js/math.js
@@ -1,6 +1,9 @@
 /* see: <https://docs.mathjax.org/en/latest/options/input/tex.html#tex-options> */
 MathJax = {
   tex: {
+    packages: {
+      "[+]": ["noundefined", "autoload", "ams", "textmacros", "physics"],
+    },
     inlineMath: [
       /* start/end delimiter pairs for in-line math */
       ["$", "$"],
@@ -11,5 +14,6 @@ MathJax = {
       ["$$", "$$"],
       ["\\[", "\\]"],
     ],
+    tags: "none", // or 'ams' or 'all'
   },
 };
