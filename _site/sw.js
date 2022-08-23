46eeffb80d593ca9e8a069f5fa5147550b437ef2
Ke Adria Dong <coliapaston@163.com>
2022-08-23 12:43:18 +0800

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220823.121649';
+const cacheName = 'chirpy-20220823.124219';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
