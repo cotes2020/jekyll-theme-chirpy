ca41c7ebffc6372447f7db245488d48c2344814c
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2022-12-09 07:03:13 +0800

    chore(deps): use `lazysizes` to load images

--- _javascript/utils/img-extra.js
+++ _javascript/utils/img-extra.js
@@ -1,6 +1,5 @@
 /**
- Lazy load images (https://github.com/ApoorvSaxena/lozad.js)
- and popup when clicked (https://github.com/dimsemenov/Magnific-Popup)
+ Set up image popup stuff (https://github.com/dimsemenov/Magnific-Popup)
  */
 
 $(function () {
@@ -9,12 +8,6 @@ $(function () {
     if ($(`${IMG_SCOPE} img`).length <= 0) {
         return;
     }
-
-    /* lazy loading */
-
-    const imgList = document.querySelectorAll(`${IMG_SCOPE} img[data-src]`);
-    const observer = lozad(imgList);
-    observer.observe();
 
     /* popup */
 
