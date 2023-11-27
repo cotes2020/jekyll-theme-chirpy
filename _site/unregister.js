6477c10fbb6861c50f1fdf9857d4baf9808a7fd1
yusufdeniz <104446207+ysf-dnz@users.noreply.github.com>
2023-11-27 19:45:51 +0300

    S

--- _site/unregister.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (let reg of registrations) {
-            reg.unregister();
-        }
-    });
-}
-
