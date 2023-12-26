dba4385185399ace50d79aee5a7ec2ac6bc613f6
 지윤 문 <jiyun0527@gmail.com>
2023-12-27 08:57:19 +0900

    revert

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703634580';
+const cacheName = 'chirpy-1703635002';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
