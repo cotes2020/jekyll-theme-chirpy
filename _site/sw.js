5d1fbefb8b1c8221532bc76e03e5241097d2ca12
 지윤 문 <jiyun0527@gmail.com>
2023-12-27 08:58:37 +0900

    gitignore edit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703635002';
+const cacheName = 'chirpy-1703635098';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
