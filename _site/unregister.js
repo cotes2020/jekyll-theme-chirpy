b5c2c8da4657e62127795c285611fc4c77cd62e6
jaehyeon-crema <jaehyeon.kim@cre.ma>
2024-01-11 22:19:07 +0900

    WIP

---
+++ _site/unregister.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let reg of registrations) {
+            reg.unregister();
+        }
+    });
+}
+
