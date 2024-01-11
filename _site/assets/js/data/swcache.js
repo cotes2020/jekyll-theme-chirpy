e99b1688b216a1d1cb6c35278fc47b60b1da0cf5
jaehyeon-crema <jaehyeon.kim@cre.ma>
2024-01-11 22:19:07 +0900

    initial commit

---
+++ _site/assets/js/data/swcache.js
@@ -0,0 +1,51 @@
+const resource = [
+  /* --- CSS --- */
+  '/assets/css/jekyll-theme-chirpy.css',
+
+  /* --- PWA --- */
+  '/app.js',
+  '/sw.js',
+
+  /* --- HTML --- */
+  '/index.html',
+  '/404.html',
+
+  
+    '/categories/',
+  
+    '/tags/',
+  
+    '/archives/',
+  
+    '/about/',
+  
+
+  /* --- Favicons & compressed JS --- */
+  
+  
+    '/assets/img/favicons/android-chrome-192x192.png',
+    '/assets/img/favicons/android-chrome-512x512.png',
+    '/assets/img/favicons/apple-touch-icon.png',
+    '/assets/img/favicons/favicon-16x16.png',
+    '/assets/img/favicons/favicon-32x32.png',
+    '/assets/img/favicons/favicon.ico',
+    '/assets/img/favicons/mstile-150x150.png'
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'localhost:4000',
+
+  
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [];
+
