fb56b851d0ec1ac2bc4f1b84f1d9942dc4264cb0
Mehmet Kerim Sarğın <89380376+kerimsarginnn@users.noreply.github.com>
2024-03-22 16:32:07 +0300

    q

--- automation.py
+++ automation.py
@@ -8,8 +8,9 @@ import os
 import time
 import xmltodict
 import json
+from flask import Flask, request, jsonify
 
-# Python3 -m pip install beautifulsoup4 html2markdown xmltodict python-dateutil --upgrade
+app = Flask(__name__)
 
 ysfdir = "."
 
@@ -19,19 +20,18 @@ def get_clean_title(title):
     first_word = words[0]
     return first_word
 
-# RSS feed URL
-rss_url = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@yusuf.deniz"
-
-while True:
+@app.route('/update', methods=['GET'])
+def update():
     try:
+        # RSS feed URL
+        rss_url = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@yusuf.deniz"
+
         # Send API request
         response = requests.get(rss_url)
 
         # Check for successful response
         if response.status_code != 200:
-            print(f"Error: Unexpected response from server - Status Code: {response.status_code}")
-            time.sleep(60*5)  # Wait for 5 minutes before trying again
-            continue
+            return jsonify({"message": f"Error: Unexpected response from server - Status Code: {response.status_code}"}), 500
 
         data = response.json()
 
@@ -47,7 +47,6 @@ while True:
             file_path = os.path.join(output_directory, file)
             if os.path.isfile(file_path):
                 os.remove(file_path)
-                print(f"Deleted file: {file_path}")
 
         books = []
         for item in items:
@@ -81,11 +80,9 @@ while True:
                 file.write(yaml_front_matter)
                 file.write(description_markdown)
 
-            print(f"Created Markdown file: {output_file_path}")
-
         # Update bookshelf.md
-        file_path = ysfdir + "/_tabs/bookshelf.md"
-        with open(file_path, 'w') as f:
+        bookshelf_file_path = ysfdir + "/_tabs/bookshelf.md"
+        with open(bookshelf_file_path, 'w') as f:
             f.write("---\n")
             f.write("# the default layout is 'page'\n")
             f.write("icon: fas fa-book\n")
@@ -95,25 +92,21 @@ while True:
                 title = book.get("title", "")
                 f.write(f"- [{title}](/posts/{get_clean_title(title=title)})\n")
 
-            print(f"Updated md file: {file_path}")
-
         # Update projects.md
         git_url = "https://api.github.com/users/ysf-dnz/repos"
-        response = requests.get(git_url)
-        file_path = ysfdir + "/_tabs/projects.md"
-        with open(file_path, 'w') as f:
+        git_response = requests.get(git_url)
+        projects_file_path = ysfdir + "/_tabs/projects.md"
+        with open(projects_file_path, 'w') as f:
             f.write("---\n")
             f.write("# the default layout is 'page'\n")
             f.write("icon: fas fa-diagram-project\n")
             f.write("order: 5\n")
             f.write("---\n\n")
-            for repo in response.json():
+            for repo in git_response.json():
                 name = repo.get("name", "")
                 description = repo.get("description", "")
                 f.write(f"- [{name}](https://github.com/ysf-dnz/{name})\n")
                 f.write(f"   {description}\n")
-
-                print(f"Updated md file: {file_path}")
 
         # Git commands
         os.system("git add .")
@@ -121,14 +114,13 @@ while True:
         os.system("git push origin master")
         print("Git push successful")
 
-        time.sleep(60*60)  # Wait for 1 hour before running again
+        return jsonify({"message": "Update successful"}), 200
 
     except requests.exceptions.RequestException as e:
-        print(f"Error: Request Exception - {e}")
-        time.sleep(60*5)  # Wait for 5 minutes before trying again
+        return jsonify({"message": f"Error: Request Exception - {e}"}), 500
 
     except json.JSONDecodeError as e:
-        print(f"Error: JSON Decode Error - {e}")
-        print(f"Response Text: {response.text}")
-        time.sleep(60*5)  # Wait for 5 minutes before trying again
+        return jsonify({"message": f"Error: JSON Decode Error - {e}\nResponse Text: {response.text}"}), 500
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
