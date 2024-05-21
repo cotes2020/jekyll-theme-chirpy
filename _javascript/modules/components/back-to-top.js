325dc04895ceb3613aec963138cfd1b3138c72e8
Alexander Fuks <kungfux@users.noreply.github.com>
2024-05-21 22:53:25 +0400

    feat: add progress indicator

--- _javascript/modules/components/back-to-top.js
+++ _javascript/modules/components/back-to-top.js
@@ -8,6 +8,15 @@ export function back2top() {
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       btn.classList.add('show');
+
+      const circumference = 2 * 3.14 * 20;
+      const circle = document.querySelector('#progress-circle circle');
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollTop / docHeight;
+      const drawLength = circumference * scrollFraction;
+
+      circle.style.strokeDashoffset = circumference - drawLength;
     } else {
       btn.classList.remove('show');
     }
