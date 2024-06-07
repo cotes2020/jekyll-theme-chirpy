f1d35832f425814cf0ee5dd3513597999bb31fc0
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2024-06-08 00:45:59 +0800

    build(devcontainer): add omz plugins & verify extension signature
    VSCode fixed signature validation hangs in v1.90

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,3 +8,11 @@ fi
 
 # Install dependencies for shfmt extension
 curl -sS https://webi.sh/shfmt | sh &>/dev/null
+
+# Add OMZ plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sed -i -E "s/^(plugins=\()(git)(\))/\1\2 zsh-syntax-highlighting zsh-autosuggestions\3/" ~/.zshrc
+
+# Avoid git log use less
+echo -e "\nunset LESS" >>~/.zshrc
