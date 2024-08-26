a71ecfcc1860bd54f4145a983f3148d52ab90803
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2024-08-26 15:39:58 +0800

    build: improve the JS build for PWA
    Customized Jekyll collections can't be passed through the gem to the user-end.

--- rollup.config.js
+++ rollup.config.js
@@ -6,35 +6,42 @@ import fs from 'fs';
 import pkg from './package.json';
 
 const SRC_DEFAULT = '_javascript';
-const DIST_DEFAULT = 'assets/js/dist';
-
 const SRC_PWA = `${SRC_DEFAULT}/pwa`;
-const DIST_PWA = '_app';
+const DIST = 'assets/js/dist';
 
 const banner = `/*!
  * ${pkg.name} v${pkg.version} | © ${pkg.since} ${pkg.author} | ${pkg.license} Licensed | ${pkg.homepage}
  */`;
 
+const frontmatter = `---\npermalink: /:basename\n---\n`;
+
 const isProd = process.env.BUILD === 'production';
 
-function cleanup(...directories) {
-  for (const dir of directories) {
-    fs.rm(dir, { recursive: true, force: true }, (err) => {
-      if (err) {
-        console.error(`Failed to remove directory ${dir}: ${err}`);
+function cleanup() {
+  fs.rmSync(DIST, { recursive: true, force: true });
+  console.log(`> Directory "${DIST}" has been cleaned.`);
+}
+
+function insertFrontmatter() {
+  return {
+    name: 'insert-frontmatter',
+    generateBundle(_, bundle) {
+      for (const chunkOrAsset of Object.values(bundle)) {
+        if (chunkOrAsset.type === 'chunk') {
+          chunkOrAsset.code = frontmatter + chunkOrAsset.code;
+        }
       }
-    });
-  }
+    }
+  };
 }
 
 function build(filename, opts = {}) {
-  const src = opts.src || SRC_DEFAULT;
-  const dist = opts.dist || DIST_DEFAULT;
+  const { src = SRC_DEFAULT, jekyll = false } = opts;
 
   return {
     input: `${src}/${filename}.js`,
     output: {
-      file: `${dist}/${filename}.min.js`,
+      file: `${DIST}/${filename}.min.js`,
       format: 'iife',
       name: 'Chirpy',
       banner,
@@ -51,12 +58,13 @@ function build(filename, opts = {}) {
       }),
       nodeResolve(),
       yaml(),
-      isProd && terser()
+      isProd && terser(),
+      jekyll && insertFrontmatter()
     ]
   };
 }
 
-cleanup(DIST_DEFAULT, DIST_PWA);
+cleanup();
 
 export default [
   build('commons'),
@@ -65,6 +73,6 @@ export default [
   build('page'),
   build('post'),
   build('misc'),
-  build('app', { src: SRC_PWA, dist: DIST_PWA }),
-  build('sw', { src: SRC_PWA, dist: DIST_PWA })
+  build('app', { src: SRC_PWA, jekyll: true }),
+  build('sw', { src: SRC_PWA, jekyll: true })
 ];
