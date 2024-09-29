320005c6cf8a11f3c84faa15dd299e8c4dfa8bd9
Alexander Fuks <kungfux@users.noreply.github.com>
2024-09-29 20:53:17 +0000

    feat: show toc on mobile screens

--- _javascript/modules/layouts.js
+++ _javascript/modules/layouts.js
@@ -1,3 +1,4 @@
 export { basic } from './layouts/basic';
 export { initSidebar } from './layouts/sidebar';
 export { initTopbar } from './layouts/topbar';
+export { initPostTopbar, initOverlay } from './components/post-topbar';
