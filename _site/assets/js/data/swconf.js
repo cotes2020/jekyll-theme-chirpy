a97ddb86f094ac7663a5af02822cd5985c25992e
Bontinck L. <85873312+MoutonDemocrate@users.noreply.github.com>
2024-10-26 02:52:23 +0200

    Auto stash before merge of "master" and "origin/master"

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729756068',resources: [
+    cacheName: 'chirpy-1729903920',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
