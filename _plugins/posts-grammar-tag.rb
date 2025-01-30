3fd279e5476c489f7d799f67d56a95cf2fdfe6d5
xinyu <x3huang@ucsd.edu>
2025-01-30 12:13:18 +0800

    🩹 changed header regex

--- _plugins/posts-grammar-tag.rb
+++ _plugins/posts-grammar-tag.rb
@@ -4,14 +4,14 @@
 
 module Jekyll
     class HeaderTagGenerator < Generator
-      priority :low
+      priority :high
   
       def generate(site)
         site.posts.docs.each do |post|
           next unless post.content
   
           # Extract headers (e.g., #### 1. **～せい**)
-          headers = post.content.scan(/^####\s+\d+\.\s+\*\*(.*?)\*\*/).flatten
+          headers = post.content.scan(/^####[\d\s\.]*\*\*(.*?)\*\*.*$/).flatten
   
           # Assign extracted headers as tags
           post.data['tags'] ||= []
@@ -22,11 +22,38 @@ module Jekyll
   
           # Ensure tags are unique
           post.data['tags'].uniq!
+        end
 
+        all_tags = site.posts.docs.flat_map { |post| post.data['tags'] }.uniq
+        all_tags.each do |tag|
+          site.pages << TagPage.new(site, tag)
         end
       end
     end
   
+    class TagPage < Jekyll::Page
+      def initialize(site, tag)
+        @site = site
+        @base = site.source
+        @dir  = 'tags'
+        @name = "#{tag}.md"
+    
+        self.process(@name)
+        
+        # Initialize data hash with default front matter
+        self.data ||= {}
+        self.data.merge!(
+          'layout' => 'tag',
+          'title' => tag,
+          'tag' => tag,
+          'permalink' => "/tags/#{tag}/"
+        )
+    
+        # Set proper output extension
+        self.ext = '.html'
+      end
+    end
+
     class HeaderLinkTagGenerator < Liquid::Tag
       def initialize(tag_name, text, tokens)
         super
