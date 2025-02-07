266b4c348abdfbb855e0a4554b846abc8835a7e6
davidazad1 <davidazad2000@gmail.com>
2025-02-07 16:23:34 +0330

    feat: add posts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738923732',resources: [
+    cacheName: 'chirpy-1738932810',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
