cce28f9488e3d8e0f8ad18b2f8d740066608359e
davidazad1 <davidazad2000@gmail.com>
2025-02-08 00:10:06 +0330

    feat: update _site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738960092',resources: [
+    cacheName: 'chirpy-1738960778',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
