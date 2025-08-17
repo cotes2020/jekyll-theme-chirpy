a6b3c6be500c382dc93774dec801c95b233b0a47
vinayak19th <vinayak1998th@Gmail.com>
2025-08-17 04:15:24 +0000

    feat: moved theme toggle next to search bar

--- _javascript/modules/components/mode-toggle.js
+++ _javascript/modules/components/mode-toggle.js
@@ -2,9 +2,9 @@
  * Add listener for theme mode toggle
  */
 
-const $toggle = document.getElementById('mode-toggle');
-
 export function modeWatcher() {
+  const $toggle = document.getElementById('mode-toggle');
+  console.log('Mode watcher initialized');
   if (!$toggle) {
     return;
   }
