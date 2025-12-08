a66803a0625d014bea1f5bf036a07baf5868fffa
dongyuanxin <yuanxin.me@gmail.com>
2025-12-08 10:23:53 +0800

    test: disable test script

--- tools/test.sh
+++ tools/test.sh
@@ -61,9 +61,9 @@ main() {
     -d "$SITE_DIR$_baseurl" -c "$_config"
 
   # test
-  bundle exec htmlproofer "$SITE_DIR" \
-    --disable-external \
-    --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+  # bundle exec htmlproofer "$SITE_DIR" \
+  #   --disable-external \
+  #   --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
 }
 
 while (($#)); do
